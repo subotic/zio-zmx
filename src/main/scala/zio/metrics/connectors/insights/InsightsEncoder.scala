ab87e7c4a4ffed473d03137b7698fb8bd4f23bfe
Ivan Subotic <400790+subotic@users.noreply.github.com>
2022-06-13 22:28:52 +0200

    add encoder (ongoing)

--- src/main/scala/zio/metrics/connectors/insights/InsightsEncoder.scala
+++ src/main/scala/zio/metrics/connectors/insights/InsightsEncoder.scala
@@ -1,8 +1,9 @@
 package zio.metrics.connectors.insights
 
 import zio._
-import zio.metrics.connectors.MetricEvent
+import zio.metrics.MetricKey
 import zio.metrics.MetricState
+import zio.metrics.connectors.MetricEvent
 
 case object InsightsEncoder {
 
@@ -14,14 +15,43 @@ case object InsightsEncoder {
   ): MetricsMessage = {
 
     event.current match {
-      case c: MetricState.Counter   => encodeCounter(c)
-      case g: MetricState.Gauge     => encodeGauge(g)
+      case c: MetricState.Counter   => encodeCounter(c, event)
+      case g: MetricState.Gauge     => encodeGauge(event.metricKey, g)
       case h: MetricState.Histogram => appendHistogram(result, event.metricKey, h)
       case s: MetricState.Summary   => appendSummary(result, event.metricKey, s)
       case f: MetricState.Frequency => appendFrequency(result, event.metricKey, f)
     }
 
-    def encodeCounter(event: MetricEven): MetricsMessage = ???
+    def encodeCounter(event: MetricEvent, c: MetricState.Counter): MetricsMessage = {
+
+      val delta: Double = event match {
+        case MetricEvent.New(_, current, _)          => current.asInstanceOf[MetricState.Counter].count
+        case MetricEvent.Unchanged(_, _, _)          => 0.0d
+        case MetricEvent.Updated(_, old, current, _) =>
+          current.asInstanceOf[MetricState.Counter].count - old.asInstanceOf[MetricState.Counter].count
+      }
+
+      MetricsMessage.CounterChange(
+        MetricKey.counter(event.metricKey.name),
+        event.timestamp,
+        c.count,
+        delta,
+      )
+    }
+
+    encodeGauge(event: MetricEvent, g: MetricState.Gauge): MetricsMessage = {
+      val delta: Double = event match {
+        case MetricEvent.New(_, current, _)          => current.asInstanceOf[MetricState.Counter].count
+        case MetricEvent.Unchanged(_, _, _)          => 0.0d
+        case MetricEvent.Updated(_, old, current, _) =>
+          current.asInstanceOf[MetricState.Counter].count - old.asInstanceOf[MetricState.Counter].count
+      }
+
+      MetricsMessage.GaugeChange(
+        
+      )
+
+    }
 
   }
 }
